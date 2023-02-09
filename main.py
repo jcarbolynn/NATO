d52a618e78ae250cb4ea18f01376cfa256683f4d
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-02-09 15:13:42 -0500

    Update main.py
    adding ability to only use characters in nato alphabet; if entry is not in the alphabet uses keyerror to tell user to make a new entry that only includes letters

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import pandas
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# not organized the way we want
-# cant do .to_dict():
-# print(data.to_dict())
-# print(type(data)
-
-# data is a data frame so we can go through it row by row with iterrows
-# get each value by column name by going into row.column_name
-# for (index, row) in data.iterrows():
-#   print(row.letter, row.code)
-
 # create a new dictionary from a dataframe, go row by row using .iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
-# use keys to get to values
-# print(phonetic_dict['K'])
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-# [new_item for item in list]
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def make_NATO_happy():
+  word = input("Enter a word: ").upper()
+  try:
+    output_list = [phonetic_dict[letter] for letter in word]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please")
+    # to repeat if user typed in something this cant interpret call function again not just this line: word = input("Enter a word: ").upper()
+    make_NATO_happy()
+  else:
+    print(output_list)
+
+make_NATO_happy()
